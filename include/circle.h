7ae4dc7b9075746bbcb876d0e05f8e9facd460c1
V-inTim <vinichuk.timofey@mail.ru>
2024-03-10 18:28:50 +0300

    add class circle

--- include/circle.h
+++ include/circle.h
@@ -1,7 +1,19 @@
-// Copyright 2022 UNN-CS
-#ifndef INCLUDE_CIRCLE_H_
-#define INCLUDE_CIRCLE_H_
-#include <cstdint>
+// Copyright 2024 Timofey Vinichuk
+#pragma once
+#define M_PI 3.14159265358979323846
 
+class circle{
+ private:
+    double _radius;
+    double _ference;
+    double _area;
 
-#endif  // INCLUDE_CIRCLE_H_
+ public:
+    explicit circle(double radius);
+    void setRadius(double radius);
+    void setFerence(double ference);
+    void setArea(double area);
+    double getRadius();
+    double getFerence();
+    double getArea();
+};
--- src/circle.cpp
+++ src/circle.cpp
@@ -1,4 +1,49 @@
-// Copyright 2022 UNN-CS
-#include <cstdint>
+// Copyright 2024 Timofey Vinichuk
+#include <cmath>
+#include <stdexcept>
+
 #include "circle.h"
 
+circle::circle(double radius) {
+    if (radius <= 0)
+        throw std::invalid_argument("radius cant be negative or null");
+    this->_radius = radius;
+    this->_ference = 2 * M_PI * radius;
+    this->_area = M_PI * radius * radius;
+}
+
+void circle::setRadius(double radius) {
+    if (radius <= 0)
+        throw std::invalid_argument("radius cant be negative or null");
+    this->_radius = radius;
+    this->_ference = 2 * M_PI * radius;
+    this->_area = M_PI * radius * radius;
+}
+
+void circle::setFerence(double ference) {
+    if (ference <= 0)
+        throw std::invalid_argument("ference cant be negative or null");
+    this->_ference = ference;
+    this->_radius = ference / (2 * M_PI);
+    this->_area = M_PI * this->_radius * this->_radius;
+}
+
+void circle::setArea(double area) {
+    if (area <= 0)
+        throw std::invalid_argument("area cant be negative or null");
+    this->_area = area;
+    this->_radius = sqrt(area / (M_PI));
+    this->_ference = 2 * M_PI * this->_radius;
+}
+
+double circle::getRadius() {
+    return this->_radius;
+}
+
+double circle::getFerence() {
+    return this->_ference;
+}
+
+double circle::getArea() {
+    return this->_area;
+}
